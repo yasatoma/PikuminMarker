8018e4d10beec789f77154211f3fb95ab1ef596c
yasatoma <yasagure.tomato@live.jp>
2022-07-25 18:48:05 +0900

    Add Shortcut command.

--- PikuminMarker/Plugin.cs
+++ PikuminMarker/Plugin.cs
@@ -9,14 +9,18 @@ using System.Reflection;
 
 using PikuminMarker.Config;
 using PikuminMarker.Service;
-
+using FFXIVClientStructs.FFXIV.Client.System.Framework;
+using FFXIVClientStructs.FFXIV.Client.Game.Object;
 
 namespace PikuminMarker
 {
-    public sealed class Plugin : IDalamudPlugin
+    public unsafe sealed class Plugin : IDalamudPlugin
     {
         public string Name => "PikuminMarker";
         private const string commandName = "/pkm";
+        private const string commandName1 = "/pkm1";
+        private const string commandName2 = "/pkm2";
+        private const string commandName3 = "/pkm3";
 
         public Configuration Configuration { get; init; }
         private readonly ConfigurationUI ConfigurationUI;
@@ -31,10 +35,29 @@ namespace PikuminMarker
             PluginLog.LogDebug(string.Format("{0} ver {1} : Start. ", Name, Assembly.GetExecutingAssembly().GetName().Version));
             PluginServices.Initialize(pluginInterface);
 
-            PluginServices.CommandManager.AddHandler(commandName, new CommandInfo(OnCommand)
-            {
-                HelpMessage = "Open NamepleteColor ConfigWindow."
-            });
+            PluginServices.CommandManager.AddHandler(
+                commandName, new CommandInfo(OnCommand)
+                {
+                    HelpMessage = "Open NamepleteColor ConfigWindow."
+                });
+
+            PluginServices.CommandManager.AddHandler(
+                commandName1, new CommandInfo(OnCommand)
+                {
+                    HelpMessage = "Add Marker1 to <t>. e.g. /pkm1 <t>"
+                });
+            PluginServices.CommandManager.AddHandler(
+                commandName2, new CommandInfo(OnCommand)
+                {
+                    HelpMessage = "Add Marker2 to <t>. e.g. /pkm2 <mo>"
+                });
+            PluginServices.CommandManager.AddHandler(
+                commandName3, new CommandInfo(OnCommand)
+                {
+                    HelpMessage = "Add Marker3 to <t>."
+                });
+
+
 
             Configuration = PluginServices.DalamudPluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
             Configuration.Initialize(PluginServices.DalamudPluginInterface);
@@ -69,8 +92,49 @@ namespace PikuminMarker
 
         private void OnCommand(string command, string args)
         {
-            // in response to the slash command, just display our main ui
-            ConfigurationUI.Visible = true;
+
+            if (command == commandName)
+            {
+                // in response to the slash command, just display our main ui
+                ConfigurationUI.Visible = true;
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(args))
+                {
+                    PluginServices.ChatGui.PrintError($"/pkm* <t>. *=1,2,3.");
+                    return;
+                }
+
+                var resolve = Framework.Instance()->GetUiModule()->GetPronounModule()->ResolvePlaceholder(args, 0, 0);
+                if (resolve == null)
+                {
+                    foreach (var actor in PluginServices.ObjectTable)
+                    {
+                        if (actor == null) continue;
+                        if (actor.Name.TextValue.Equals(args, StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            resolve = (GameObject*)actor.Address;
+                            break;
+                        }
+                    }
+                }
+                if (resolve != null && resolve->ObjectKind == 1 && resolve->SubKind == 4)
+                {
+                    switch (command)
+                    {
+                        case commandName1:
+                            this.Configuration.Target1ObjectID = resolve->ObjectID;
+                            break;
+                        case commandName2:
+                            this.Configuration.Target2ObjectID = resolve->ObjectID;
+                            break;
+                        case commandName3:
+                            this.Configuration.Target3ObjectID = resolve->ObjectID;
+                            break;
+                    }
+                }
+            }
         }
 
         private void DrawUI()
